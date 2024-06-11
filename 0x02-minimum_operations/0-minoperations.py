e29b7b924b22cd5a80aa843e66e631e65714b4ac
oswealth <osbertjaywealth@gmail.com>
2024-06-11 21:46:19 +0000

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module for calculating minimum operations to achieve 'n' characters.
+"""
+
+def minOperations(n):
+    """
+    Calculate the minimum number of operations to get exactly 'n' 'H' characters in the file.
+    :param n: int
+    :return: int
+    """
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
