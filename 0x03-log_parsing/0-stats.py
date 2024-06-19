b105eedcb96a1544caa8109421620d88c7db1841
oswealth <osbertjaywealth@gmail.com>
2024-06-19 18:16:55 +0000

    Write a script that reads stdin line by line and computes metrics

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+This module contains a method that reads stdin line by line and
+computes metrics.
+"""
+import sys
+
+def display_metrics(total_size, status_code):
+    """
+    Function that prints the metrics.
+    """
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_code.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
+
+if __name__ == '__main__':
+    total_size = 0
+    status_code = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+
+    try:
+        i = 0
+        for line in sys.stdin:
+            args = line.split()
+            if len(args) > 6:
+                status = args[-2]
+                file_size = args[-1]
+                try:
+                    total_size += int(file_size)
+                    if status in status_code:
+                        status_code[status] += 1
+                except ValueError:
+                    continue
+                i += 1
+                if i % 10 == 0:
+                    display_metrics(total_size, status_code)
+
+    except KeyboardInterrupt:
+        display_metrics(total_size, status_code)
+        raise
+    display_metrics(total_size, status_code)
