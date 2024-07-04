50365707ba2a0612ac02f531a52b98c66c78f19a
oswealth <osbertjaywealth@gmail.com>
2024-07-04 05:08:30 +0000

    N queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Module that solves the N Queens puzzle
+"""
+from sys import argv, exit
+
+
+def solve_queens(row, n, solution):
+    """
+    Solves the N Queens problem using backtracking
+    """
+    if row == n:
+        print(solution)
+    else:
+        for col in range(n):
+            placement = [row, col]
+            if valid_placement(solution, placement):
+                solution.append(placement)
+                solve_queens(row + 1, n, solution)
+                solution.pop()
+
+
+def valid_placement(solution, placement):
+    """
+    Checks if the current placement is valid
+    """
+    for queen in solution:
+        if queen[1] == placement[1]:
+            return False
+        if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+            return False
+        if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        n = int(argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    solution = []
+    solve_queens(0, n, solution)
