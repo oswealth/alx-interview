8ae5834ffd38e51b63bd83267e01e31192429393
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-07-24 20:41:56 +0000

    making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,20 +2,22 @@
 """A function to determine the fewest number of coins needed
    to meet a given amount total"""
 
-   def makeChange(coins, total):
-        """
-	   This function will take a list of coins and use
-           that to calculate how much change the total will require
-        """
-	if total <= 0:
-	   return 0
 
-        coins.sort(reverse=True)  # Sort coins in descending order
+def makeChange(coins, total):
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
+    """
+    if total <= 0:
+        return 0
+
+    else:
+        coin = sorted(coins)
+        coin.reverse()
         counter = 0
-
-        for coin in coins:
-           while total >= coin:
-               counter += 1
-               total -= coin
-
-        return counter if total == 0 else -1
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
